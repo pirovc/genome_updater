87a81474e74f0317742821404f83bd61472d2225
pirovc <pirovc@posteo.net>
2025-11-17 13:36:55 +0100

    Fix coverage reports

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# apt-get install shellcheck
 if shellcheck genome_updater.sh
 then
     echo "shellcheck found no issues!"
 fi
 
+# mamba install ruby
+# gem install bashcov codecov
+# To generate local html coverage reports
 echo -e "SimpleCov.start do\n  add_filter 'tests/'\nend" > .simplecov
 bashcov --skip-uncovered tests/libs/bats/bin/bats tests/integration_offline.bats
