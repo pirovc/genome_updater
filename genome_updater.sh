971d68b662e1bf9ccc425ade02c1d3dd71ca300b
pirovc <pirovc@posteo.net>
2026-03-12 15:35:35 +0100

    Bump version

--- genome_updater.sh
+++ genome_updater.sh
@@ -25,7 +25,7 @@ IFS=$' '
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-version="0.7.1"
+version="0.7.2"
 
 # Define ncbi_base_url or use local files (for testing)
 local_dir=${local_dir:-}
